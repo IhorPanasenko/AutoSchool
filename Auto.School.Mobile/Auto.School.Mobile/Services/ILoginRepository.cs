b27380e41ab2052ce0debc58697537b8691a7566
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-02-14 00:22:39 +0200

    Create Initial Login Service

---
+++ Auto.School.Mobile/Auto.School.Mobile/Services/ILoginRepository.cs
@@ -0,0 +1,14 @@
+﻿using Auto.School.Mobile.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto.School.Mobile.Services
+{
+    public interface ILoginRepository
+    {
+        Task<UserInfo> Login(string username, string password);
+    }
+}
---
+++ Auto.School.Mobile/Auto.School.Mobile/Services/LoginService.cs
@@ -0,0 +1,18 @@
+﻿using Auto.School.Mobile.Models;
+
+namespace Auto.School.Mobile.Services
+{
+    internal class LoginService : ILoginRepository
+    {
+
+        public async Task<UserInfo> Login(string username, string password)
+        {
+            return new UserInfo
+            {
+                Password = password,
+                UserName = username,
+                UserId = 1
+            };
+        }
+    }
+}
