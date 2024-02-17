19956852434589f6d4c27d12fd35d2ccc1d14932
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-02-17 16:38:16 +0100

    Modularize login controller into `authController`

--- auto-school-server/src/controllers/authController.js
+++ auto-school-server/src/controllers/authController.js
@@ -34,3 +34,23 @@ exports.signup = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      res.status(400).json({ message: 'Provide email and password' });
+    }
+
+    const user = await userLogin.findOne({ email });
+
+    //TODO: verify password
+
+    //TODO: sign jwt tokens
+
+    //TODO: save refresh token in db
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
--- auto-school-server/src/routes/authRoutes.js
+++ auto-school-server/src/routes/authRoutes.js
@@ -9,24 +9,6 @@ const router = express.Router();
 
 router.post('/signup', validateSchema(signupSchema), authController.signup);
 
-router.post('/login', async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    if (!email || !password) {
-      res.status(400).json({ message: 'Provide email and password' });
-    }
-
-    const user = await userLogin.findOne({ email });
-
-    //TODO: verify password
-
-    //TODO: sign jwt tokens
-
-    //TODO: save refresh token in db
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+router.post('/login', authController.login);
 
 module.exports = router;
