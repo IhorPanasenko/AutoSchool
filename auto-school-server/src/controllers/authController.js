d862a7a5b04fb817a35b9963c089499c9381ccf2
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-02-21 18:46:01 +0100

    Extract code for signing and saving tokens in cookies into function

--- auto-school-server/src/controllers/authController.js
+++ auto-school-server/src/controllers/authController.js
@@ -2,6 +2,31 @@ const jwt = require('jsonwebtoken');
 const userLogin = require('../models/userLogin');
 const userAccount = require('../models/userAccount.js');
 
+const signSaveTokens = (res, userId, role) => {
+  const accessToken = jwt.sign(
+    { userId, role },
+    process.env.JWT_ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.JWT_ACCESS_TOKEN_EXPIRES }
+  );
+  const refreshToken = jwt.sign(
+    { userId },
+    process.env.JWT_REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.JWT_REFRESH_TOKEN_EXPIRES }
+  );
+
+  res.cookie('access_token', accessToken, {
+    maxAge: process.env.JWT_ACCESS_TOKEN_COOKIE_EXPIRES * 1000,
+    httpOnly: true,
+  });
+
+  res.cookie('refresh_token', refreshToken, {
+    maxAge: process.env.JWT_REFRESH_TOKEN_COOKIE_EXPIRES * 60 * 1000,
+    httpOnly: true,
+  });
+
+  return refreshToken;
+};
+
 exports.signup = async (req, res) => {
   try {
     const newUserLogin = await userLogin.create({
@@ -21,26 +46,11 @@ exports.signup = async (req, res) => {
 
     // TODO: Email validation
 
-    const accessToken = jwt.sign(
-      { userId: newUserAccount.userId, role: newUserAccount.role },
-      process.env.JWT_ACCESS_TOKEN_SECRET,
-      { expiresIn: process.env.JWT_ACCESS_TOKEN_EXPIRES }
+    const refreshToken = signSaveTokens(
+      res,
+      newUserAccount.userId,
+      newUserAccount.role
     );
-    const refreshToken = jwt.sign(
-      { userId: newUserAccount.userId },
-      process.env.JWT_REFRESH_TOKEN_SECRET,
-      { expiresIn: process.env.JWT_REFRESH_TOKEN_EXPIRES }
-    );
-
-    res.cookie('access_token', accessToken, {
-      maxAge: process.env.JWT_ACCESS_TOKEN_COOKIE_EXPIRES * 1000,
-      httpOnly: true,
-    });
-
-    res.cookie('refresh_token', refreshToken, {
-      maxAge: process.env.JWT_REFRESH_TOKEN_COOKIE_EXPIRES * 60 * 1000,
-      httpOnly: true,
-    });
 
     newUserLogin.refreshToken = refreshToken;
     newUserLogin.save();
@@ -74,6 +84,7 @@ exports.login = async (req, res) => {
     //TODO: sign jwt tokens
 
     //TODO: save refresh token in db
+
     res.status(200).json({ message: 'success' });
   } catch (err) {
     res.status(500).json({ message: err.message });
