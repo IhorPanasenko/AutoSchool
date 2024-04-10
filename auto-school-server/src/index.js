2a80b647658ac16aa5698c7eae947bfffa7bdca8
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-04-10 17:59:09 +0200

    Refactor global error handler to its own module

--- auto-school-server/src/index.js
+++ auto-school-server/src/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const AppError = require('./helpers/appError.js');
+const globalErrorHandler = require('./controllers/errorController.js');
 const userRouter = require('./routes/userRoutes.js');
 const authRouter = require('./routes/authRoutes.js');
 const cityRouter = require('./routes/cityRoutes.js');
@@ -25,14 +26,7 @@ app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
