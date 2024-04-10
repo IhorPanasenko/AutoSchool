c3af56873c2961fc525e2d3d99f20001710c3f62
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-04-10 17:43:22 +0200

    Implement global error handling middleware

--- auto-school-server/src/index.js
+++ auto-school-server/src/index.js
@@ -20,10 +20,19 @@ app.use('/api/auth', authRouter);
 app.use('/api/cities/', cityRouter);
 app.use('/api/instructors', instructorRouter);
 
-app.all('*', (req, res) => {
-  res.status(404).json({
-    status: 'failure',
-    message: `Can't find ${req.originalUrl} on this server`,
+app.all('*', (req, res, next) => {
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
