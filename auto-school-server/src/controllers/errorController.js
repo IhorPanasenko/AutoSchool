ca0d4f847c937ae1d3e5c283e1c881e50490ffeb
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-04-12 21:39:38 +0200

    Implement production and development errors

--- auto-school-server/src/controllers/errorController.js
+++ auto-school-server/src/controllers/errorController.js
@@ -1,8 +1,27 @@
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+
+  if (process.env.NODE_ENV === 'production') {
+    if (err.isOperational) {
+      res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+      });
+    } else {
+      console.error('ERROR OCCURED: ', err);
+
+      res.status(500).json({
+        status: 'error',
+        message: 'Something went wrong.',
+      });
+    }
+  } else {
+    res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack,
+    });
+  }
 };
