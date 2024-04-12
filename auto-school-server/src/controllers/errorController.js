78d92ffaeb8a5cff8d33a603b2a9e8177eedd99b
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-04-12 21:45:56 +0200

    Refactor error controller

--- auto-school-server/src/controllers/errorController.js
+++ auto-school-server/src/controllers/errorController.js
@@ -3,25 +3,33 @@ module.exports = (err, req, res, next) => {
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'production') {
-    if (err.isOperational) {
-      res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
-      });
-    } else {
-      console.error('ERROR OCCURED: ', err);
-
-      res.status(500).json({
-        status: 'error',
-        message: 'Something went wrong.',
-      });
-    }
+    productionError(err, res);
   } else {
+    developmentError(err, res);
+  }
+};
+
+const developmentError = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const productionError = (err, res) => {
+  if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
-      error: err,
       message: err.message,
-      stack: err.stack,
+    });
+  } else {
+    console.error('ERROR OCCURED: ', err);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong.',
     });
   }
 };
