107302dcbe0c080473fa0007dbe5e97ab446a17a
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-04-14 13:21:55 +0200

    Handle `CastError` Mongodb error

--- auto-school-server/src/controllers/errorController.js
+++ auto-school-server/src/controllers/errorController.js
@@ -1,12 +1,24 @@
+const AppError = require('./../helpers/appError.js');
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'production') {
-    productionError(err, res);
+    let error = { ...err };
+
+    if (err.name === 'CastError') {
+      error = handleCastErrorMongoDB(error);
+    }
+
+    productionError(error, res);
   } else {
     developmentError(err, res);
   }
+};
+
+const handleCastErrorMongoDB = (err) => {
+  return new AppError(`Incorrect ${err.path}: ${err.value}`, 400);
 };
 
 const developmentError = (err, res) => {
@@ -25,7 +37,7 @@ const productionError = (err, res) => {
       message: err.message,
     });
   } else {
-    console.error('ERROR OCCURED: ', err);
+    //console.error('ERROR OCCURED: ', err);
 
     res.status(500).json({
       status: 'error',
