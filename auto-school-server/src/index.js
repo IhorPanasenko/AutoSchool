80a7922874343960b24f873f08d32a4d940dfea3
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-04-14 15:48:22 +0200

    Handle unhandledRejections

--- auto-school-server/src/index.js
+++ auto-school-server/src/index.js
@@ -28,6 +28,14 @@ app.all('*', (req, res, next) => {
 
 app.use(globalErrorHandler);
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
