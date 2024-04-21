efe67d3d40cc67604b98fa3c3d57ab7820d469a8
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-04-22 00:10:41 +0300

    Create ErrorAlertView

--- Auto.School.Mobile/Auto.School.Mobile/Shared/Views/Alerts/ErrorAlertView.xaml.cs
+++ Auto.School.Mobile/Auto.School.Mobile/Shared/Views/Alerts/ErrorAlertView.xaml.cs
@@ -1,3 +1,5 @@
+using CommunityToolkit.Mvvm.Input;
+
 namespace Auto.School.Mobile.Shared.Alerts;
 
 public partial class ErrorAlertView : ContentView
@@ -6,4 +8,49 @@ public partial class ErrorAlertView : ContentView
 	{
 		InitializeComponent();
 	}
+
+    public static readonly BindableProperty IsErrorProperty =
+        BindableProperty.Create(
+            nameof(IsError),
+            typeof(bool),
+            typeof(ErrorAlertView),
+            false);
+
+    public bool IsError 
+	{
+		get => (bool)GetValue(IsErrorProperty);
+		set => SetValue(IsErrorProperty, value); 
+	}
+
+	public static readonly BindableProperty ErrorMessageProperty =
+		BindableProperty.Create(
+			nameof(ErrorMessage),
+			typeof(string),
+			typeof(ErrorAlertView),
+			string.Empty);
+		
+	public string ErrorMessage
+	{
+		get => (string)GetValue(ErrorMessageProperty);
+		set => SetValue(ErrorMessageProperty, value);
+	}
+
+    //public static readonly BindableProperty CloseActionProperty =
+    //        BindableProperty.Create(
+    //            nameof(CloseAction),
+    //            typeof(Action),
+    //            typeof(ErrorAlertView),
+    //            null);
+
+    //public Action CloseAction
+    //{
+    //    get => (Action)GetValue(CloseActionProperty);
+    //    set => SetValue(CloseActionProperty, value);
+    //}
+
+    private void CloseButton_Clicked(object sender, EventArgs e)
+    {
+        IsError = false;
+        ErrorMessage = string.Empty;
+    }
 }
