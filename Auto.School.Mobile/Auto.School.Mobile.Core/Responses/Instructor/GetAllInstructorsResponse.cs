34a18a59b3f54fdbd1e8379930a53bb563f9057d
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-04-26 00:51:01 +0300

    Create GetAllInstructorsResponse.cs

---
+++ Auto.School.Mobile/Auto.School.Mobile.Core/Responses/Instructor/GetAllInstructorsResponse.cs
@@ -0,0 +1,11 @@
+﻿using Auto.School.Mobile.Core.Models;
+using Newtonsoft.Json;
+
+namespace Auto.School.Mobile.Core.Responses.Instructor
+{
+    public class GetAllInstructorsResponse : BaseResponse
+    {
+        [JsonProperty("data")]
+        public List<InstructorModel>  Instructors { get; set; } = [];
+    }
+}
