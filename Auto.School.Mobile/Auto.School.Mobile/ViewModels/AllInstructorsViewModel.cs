31bcae707071e93f6cad6c902642a7acbdd67ea2
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-04-26 00:05:53 +0300

    Create AllInstructorsViewModel.cs

---
+++ Auto.School.Mobile/Auto.School.Mobile/ViewModels/AllInstructorsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Auto.School.Mobile.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace Auto.School.Mobile.ViewModels
+{
+    public partial class AllInstructorsViewModel : BaseViewModel, INotifyPropertyChanged
+    {
+        public AllInstructorsViewModel()
+        {
+            
+        }
+
+        private void LoadInstructors()
+        {
+            
+        }
+
+        [ObservableProperty]
+        List<InstructorModel> instructors ;
+    }
+}
