16f751a0edf43c43a966ea5aaf49f471788dbad1
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-04-26 00:52:01 +0300

    Update AllInstructorsViewModel.cs

--- Auto.School.Mobile/Auto.School.Mobile/ViewModels/AllInstructorsViewModel.cs
+++ Auto.School.Mobile/Auto.School.Mobile/ViewModels/AllInstructorsViewModel.cs
@@ -1,23 +1,41 @@
-﻿using Auto.School.Mobile.Models;
+﻿using Auto.School.Mobile.Core.Constants;
+using Auto.School.Mobile.Core.Models;
+using Auto.School.Mobile.Service.Interfaces;
 using CommunityToolkit.Mvvm.ComponentModel;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
 
 namespace Auto.School.Mobile.ViewModels
 {
     public partial class AllInstructorsViewModel : BaseViewModel, INotifyPropertyChanged
     {
-        public AllInstructorsViewModel()
+        private readonly IInstructorService _instructorService;
+        public AllInstructorsViewModel(IInstructorService instructorService)
         {
-            
+            _instructorService = instructorService;
+            LoadInstructors();
         }
 
-        private void LoadInstructors()
+        private async Task LoadInstructors()
         {
-            
+            var response = await _instructorService.GetAll();
+
+            if (!string.Equals(response.Status, "success", StringComparison.InvariantCultureIgnoreCase))
+            {
+                ErrorMessage = response.Message ?? AppErrorMessagesConstants.SomethingWentWrongErrorMessage;
+                IsError = true;
+                return;
+            }
+
+            Instructors = response.Instructors;
         }
 
         [ObservableProperty]
-        List<InstructorModel> instructors ;
+        private string errorMessage = string.Empty;
+
+        [ObservableProperty]
+        private bool isError = false;
+
+        [ObservableProperty]
+        List<InstructorModel> instructors;
     }
 }
