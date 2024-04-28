452b345bab9a34e9de74eb44e2455059b47133cb
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-04-29 00:48:19 +0300

    CreateSharedService

---
+++ Auto.School.Mobile/Auto.School.Mobile/Services/SharedService.cs
@@ -0,0 +1,26 @@
+﻿using Auto.School.Mobile.Abstract;
+
+namespace Auto.School.Mobile.Services
+{
+    public class SharedService : ISharedService
+    {
+        private Dictionary<string, object> DTODict { get; set; } = [];
+
+        public void Add<T>(string key, T value) where T : class
+        {
+            if (DTODict.ContainsKey(key))
+            {
+                DTODict[key] = value;
+            }
+            else
+            {
+                DTODict.Add(key, value);
+            }
+        }
+
+        public T? GetValue<T>(string key) where T : class
+        {
+            return DTODict.ContainsKey(key) ? DTODict[key] as T : null;
+        }
+    }
+}
