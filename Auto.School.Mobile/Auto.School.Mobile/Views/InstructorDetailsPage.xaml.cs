68f59980700d3755600171ecc5473e6bfeb51d98
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-04-29 00:51:49 +0300

    Update InstructorDetailsPage.xaml.cs

--- Auto.School.Mobile/Auto.School.Mobile/Views/InstructorDetailsPage.xaml.cs
+++ Auto.School.Mobile/Auto.School.Mobile/Views/InstructorDetailsPage.xaml.cs
@@ -1,9 +1,12 @@
+using Auto.School.Mobile.ViewModels;
+
 namespace Auto.School.Mobile.Views;
 
 public partial class InstructorDetailsPage : ContentPage
 {
-	public InstructorDetailsPage()
+	public InstructorDetailsPage(InstructorDetailsViewModel instructorDetailsViewModel)
 	{
 		InitializeComponent();
+		BindingContext = instructorDetailsViewModel;
 	}
 }
