135da5f8109e2d8fa42685982e09e72283e519d7
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-04-29 00:36:14 +0300

    Create instructorDetails elements

---
+++ Auto.School.Mobile/Auto.School.Mobile/ViewModels/InstructorDetailsViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Auto.School.Mobile.Core.Models;
+using Auto.School.Mobile.Service.Interfaces;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel;
+
+namespace Auto.School.Mobile.ViewModels
+{
+    public partial class InstructorDetailsViewModel : BaseViewModel, INotifyPropertyChanged
+    {
+        private readonly IInstructorService _instructorService;
+
+        public InstructorDetailsViewModel(IInstructorService instructorService)
+        {
+            _instructorService = instructorService;
+            LoadInstructor();
+        }
+
+        private async Task LoadInstructor ()
+        {
+            Instructor = await _instructorService.GetInstructorAsync(instructorId);
+        }
+
+        [ObservableProperty]
+        private InstructorModel instructor;
+        
+    }
+}
---
+++ Auto.School.Mobile/Auto.School.Mobile/Views/InstructorDetailsPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Auto.School.Mobile.Views;
+
+public partial class InstructorDetailsPage : ContentPage
+{
+	public InstructorDetailsPage()
+	{
+		InitializeComponent();
+	}
+}
