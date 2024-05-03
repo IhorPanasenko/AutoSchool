44bbefcd9cc3459352b88a20c688088b09b4ca3b
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-05-04 01:12:11 +0300

    Create HttpClientService

---
+++ Auto.School.Mobile/Auto.School.Mobile.ApiIntegration/Servicecs/Implementation/HttpClientService.cs
@@ -0,0 +1,20 @@
+﻿using Auto.School.Mobile.ApiIntegration.Servicecs.Abstract;
+using System.Net;
+
+namespace Auto.School.Mobile.ApiIntegration.Servicecs.Implementation
+{
+    public class HttpClientService : IHttpClientService
+    {
+        private static readonly CookieContainer _cookieContainer = new CookieContainer();
+        public HttpClient Client { get; }
+
+        public HttpClientService()
+        {
+            HttpClientHandler handler = new HttpClientHandler();
+            handler.CookieContainer = _cookieContainer;
+            handler.UseCookies = true;
+
+            Client = new HttpClient(handler);
+        }
+    }
+}
