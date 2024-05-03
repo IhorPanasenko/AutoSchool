ab50854ad89ff58ec9649e858ee85da2f5360309
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-05-04 01:11:39 +0300

    Rewrite Instructor Request

---
+++ Auto.School.Mobile/Auto.School.Mobile.ApiIntegration/Requests/Abstract/IInstructorRequest.cs
@@ -0,0 +1,9 @@
+﻿using Auto.School.Mobile.Core.Responses.Instructor;
+
+namespace Auto.School.Mobile.ApiIntegration.Requests.Abstract
+{
+    public interface IInstructorRequest
+    {
+        public Task<GetAllInstructorsResponse> GetAll();
+    }
+}
---
+++ Auto.School.Mobile/Auto.School.Mobile.ApiIntegration/Requests/Implementation/InstructorRequests.cs
@@ -0,0 +1,26 @@
+﻿using Auto.School.Mobile.ApiIntegration.Base.Abstract;
+using Auto.School.Mobile.ApiIntegration.Base.Implementation;
+using Auto.School.Mobile.ApiIntegration.Constants;
+using Auto.School.Mobile.ApiIntegration.Requests.Abstract;
+using Auto.School.Mobile.Core.Responses.Instructor;
+
+namespace Auto.School.Mobile.ApiIntegration.Requests.Implementation
+{
+    public class InstructorRequests(IGetRequest getRequest) : IInstructorRequest
+    {
+        private readonly IGetRequest _getRequest = getRequest;
+
+        public async Task<GetAllInstructorsResponse> GetAll()
+        {
+            var instructorsResponse = await _getRequest.ExecuteAsync<GetAllInstructorsResponse>(RoutesConstants.GetAllInstructors);
+
+            if (instructorsResponse.Status is null)
+            {
+                instructorsResponse.Status = "fail";
+                instructorsResponse.Instructors = [];
+            }
+
+            return instructorsResponse;
+        }
+    }
+}
