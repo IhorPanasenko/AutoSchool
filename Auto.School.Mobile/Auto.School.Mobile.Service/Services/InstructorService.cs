89560cef810df7e824ad869c65ea682c947ebe55
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-05-04 01:12:45 +0300

    Update InstructorService.cs

--- Auto.School.Mobile/Auto.School.Mobile.Service/Services/InstructorService.cs
+++ Auto.School.Mobile/Auto.School.Mobile.Service/Services/InstructorService.cs
@@ -1,15 +1,16 @@
-﻿using Auto.School.Mobile.ApiIntegration.Requests;
+﻿using Auto.School.Mobile.ApiIntegration.Requests.Abstract;
 using Auto.School.Mobile.Core.Responses.Instructor;
 using Auto.School.Mobile.Service.Interfaces;
 
 namespace Auto.School.Mobile.Service.Services
 {
-    public class InstructorService : IInstructorService
+    public class InstructorService(IInstructorRequest instructorRequest) : IInstructorService
     {
-       
+        private readonly IInstructorRequest _instructorRequest = instructorRequest;
+
         public async Task<GetAllInstructorsResponse> GetAll()
         {
-            var response = await InstructorRequests.GetAll();
+            var response = await _instructorRequest.GetAll();
 
             if (response.Instructors is null)
             {
