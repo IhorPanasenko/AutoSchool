a3df8870e2253e4a897250904e93b7540f066308
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-05-04 01:13:51 +0300

    Register dependencies

--- Auto.School.Mobile/Auto.School.Mobile/MauiProgram.cs
+++ Auto.School.Mobile/Auto.School.Mobile/MauiProgram.cs
@@ -6,6 +6,12 @@ using Auto.School.Mobile.Service.Services;
 using Auto.School.Mobile.Shared.Alerts;
 using Auto.School.Mobile.Abstract;
 using Auto.School.Mobile.Services;
+using Auto.School.Mobile.ApiIntegration.Servicecs.Abstract;
+using Auto.School.Mobile.ApiIntegration.Servicecs.Implementation;
+using Auto.School.Mobile.ApiIntegration.Base.Abstract;
+using Auto.School.Mobile.ApiIntegration.Base.Implementation;
+using Auto.School.Mobile.ApiIntegration.Requests.Abstract;
+using Auto.School.Mobile.ApiIntegration.Requests.Implementation;
 
 namespace Auto.School.Mobile
 {
@@ -23,13 +29,21 @@ namespace Auto.School.Mobile
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
-            builder.Services.AddSingleton<HomePage>();
-            builder.Services.AddSingleton<LoginPage>();
-            builder.Services.AddSingleton<RegistrationPage>();
-            builder.Services.AddSingleton<ContactPage>();
-            builder.Services.AddSingleton<AboutPage>();
-            builder.Services.AddSingleton<AllInstructorsPage>();
-            builder.Services.AddSingleton<InstructorDetailsPage>();
+            builder.Services.AddSingleton<IHttpClientService, HttpClientService>();
+            builder.Services.AddSingleton<IGetRequest, GetRequest>();
+            builder.Services.AddSingleton<IPostRequest, PostRequest>();
+            builder.Services.AddSingleton<IPatchRequest, PatchRequest>();
+
+            builder.Services.AddTransient<IAuthenticationRequest, AuthenticationRequests>();
+            builder.Services.AddTransient<ICityRequest, CityRequests>();
+            builder.Services.AddTransient<IInstructorRequest, InstructorRequests>();
+            builder.Services.AddTransient<IStudentRequest, StudentRequests>();
+
+            builder.Services.AddTransient<IAuthenticationService, AuthenticationService>();
+            builder.Services.AddTransient<ICityService, CityService>();
+            builder.Services.AddTransient<IInstructorService, InstructorService>();
+            builder.Services.AddSingleton<ISharedService, SharedService>();
+            builder.Services.AddTransient<IStudentService, StudentService>();
 
             builder.Services.AddSingleton<ErrorAlertView>();
 
@@ -39,10 +53,13 @@ namespace Auto.School.Mobile
             builder.Services.AddSingleton<AllInstructorsViewModel>();
             builder.Services.AddSingleton<InstructorDetailsViewModel>();
 
-            builder.Services.AddTransient<IAuthenticationService, AuthenticationService>();
-            builder.Services.AddTransient<ICityService, CityService>();   
-            builder.Services.AddTransient<IInstructorService, InstructorService>();
-            builder.Services.AddSingleton<ISharedService, SharedService>();
+            builder.Services.AddSingleton<HomePage>();
+            builder.Services.AddSingleton<LoginPage>();
+            builder.Services.AddSingleton<RegistrationPage>();
+            builder.Services.AddSingleton<ContactPage>();
+            builder.Services.AddSingleton<AboutPage>();
+            builder.Services.AddSingleton<AllInstructorsPage>();
+            builder.Services.AddSingleton<InstructorDetailsPage>();
 #if DEBUG
             builder.Logging.AddDebug();
 #endif
