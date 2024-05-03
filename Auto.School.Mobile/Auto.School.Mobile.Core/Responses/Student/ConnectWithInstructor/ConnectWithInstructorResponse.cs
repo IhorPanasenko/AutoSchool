f67c07abfe94813df8b1d700c2d4334a19295712
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-05-04 01:12:20 +0300

    Create ConnectWithInstructorResponse.cs

---
+++ Auto.School.Mobile/Auto.School.Mobile.Core/Responses/Student/ConnectWithInstructor/ConnectWithInstructorResponse.cs
@@ -0,0 +1,8 @@
+﻿using Auto.School.Mobile.Core.Responses.Base;
+
+namespace Auto.School.Mobile.Core.Responses.Student.ConnectWithInstructor
+{
+    public class ConnectWithInstructorResponse : BaseResponse
+    {
+    }
+}
