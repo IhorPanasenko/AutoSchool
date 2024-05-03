b526c398d9b007095a9bb129adc410d751901586
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-05-04 01:10:55 +0300

    Rewrite AuthenticationRequest

---
+++ Auto.School.Mobile/Auto.School.Mobile.ApiIntegration/Requests/Abstract/IAuthenticationRequest.cs
@@ -0,0 +1,12 @@
+﻿using Auto.School.Mobile.Core.Models;
+using Auto.School.Mobile.Core.Responses.Authentication;
+using Auto.School.Mobile.Core.Responses.Login;
+
+namespace Auto.School.Mobile.ApiIntegration.Requests.Abstract
+{
+    public interface IAuthenticationRequest
+    {
+        public Task<LoginResponse> Login(LoginModel loginModel);
+        public Task<RegistrationResponse> Register(RegistrationModel registrationModel);
+    }
+}
