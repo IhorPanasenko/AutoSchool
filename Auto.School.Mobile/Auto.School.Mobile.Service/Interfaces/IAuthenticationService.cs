b23b4e9cbe99c70e7695599270ba61482cd28269
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-05-18 17:52:31 +0300

    Add ForgotPassword to AuthenticationService

--- Auto.School.Mobile/Auto.School.Mobile.Service/Interfaces/IAuthenticationService.cs
+++ Auto.School.Mobile/Auto.School.Mobile.Service/Interfaces/IAuthenticationService.cs
@@ -1,5 +1,6 @@
 ﻿using Auto.School.Mobile.Core.Models;
 using Auto.School.Mobile.Core.Responses.Authentication;
+using Auto.School.Mobile.Core.Responses.Base;
 using Auto.School.Mobile.Core.Responses.Login;
 
 namespace Auto.School.Mobile.Service.Interfaces
@@ -9,5 +10,7 @@ namespace Auto.School.Mobile.Service.Interfaces
         public Task<LoginResponse> LoginAsync(LoginModel loginModel);
 
         public Task<RegistrationResponse> RegisterAsync(RegistrationModel registrationModel);
+
+        public Task<BaseResponse> ForgotPassword(string email);
     }
 }
