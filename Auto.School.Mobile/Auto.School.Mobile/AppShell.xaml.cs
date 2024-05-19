4035105ab7a210841b54084ab97c147d340e52b2
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-05-19 13:14:47 +0300

    Update AppShell.xaml.cs

--- Auto.School.Mobile/Auto.School.Mobile/AppShell.xaml.cs
+++ Auto.School.Mobile/Auto.School.Mobile/AppShell.xaml.cs
@@ -5,10 +5,10 @@ namespace Auto.School.Mobile
 {
     public partial class AppShell : Shell
     {
-        public AppShell()
+        public AppShell(AppShellViewModel appShellViewModel)
         {
             InitializeComponent();
-            BindingContext = new AppShellViewModel();
+            BindingContext = appShellViewModel;
             Routing.RegisterRoute(nameof(HomePage), typeof(HomePage));
             Routing.RegisterRoute(nameof(LoginPage), typeof(LoginPage));
             Routing.RegisterRoute(nameof(RegistrationPage), typeof(RegistrationPage));
