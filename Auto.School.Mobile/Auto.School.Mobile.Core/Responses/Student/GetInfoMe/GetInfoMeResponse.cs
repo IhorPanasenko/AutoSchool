91cc29bbc3139291b6976a879402741cd43e2536
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-05-19 13:13:56 +0300

    Create GetInfoMeResponse.cs

---
+++ Auto.School.Mobile/Auto.School.Mobile.Core/Responses/Student/GetInfoMe/GetInfoMeResponse.cs
@@ -0,0 +1,12 @@
+﻿using Auto.School.Mobile.Core.Models;
+using Auto.School.Mobile.Core.Responses.Base;
+using Newtonsoft.Json;
+
+namespace Auto.School.Mobile.Core.Responses.Student.GetInfoMe
+{
+    public class GetInfoMeResponse : BaseResponse
+    {
+        [JsonProperty("data")]
+        public StudentModel Student { get; set; }
+    }
+}
