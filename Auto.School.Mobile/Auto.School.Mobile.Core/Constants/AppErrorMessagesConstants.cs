900bc6dbe27a4ecb44ed6d1c2dcf67514b4c3969
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-05-19 13:13:40 +0300

    Add error constants

--- Auto.School.Mobile/Auto.School.Mobile.Core/Constants/AppErrorMessagesConstants.cs
+++ Auto.School.Mobile/Auto.School.Mobile.Core/Constants/AppErrorMessagesConstants.cs
@@ -7,5 +7,6 @@
         public const string TooYoungError = "You must be elder than 18 to get access to driving license classes";
         public const string InvalidPhoneNumber = "PLease, enter a phone number in format: XXX-XXX-XXXX";
         public const string NotEqualPasswords = "Entered passwords are not equal";
+        public const string FailedToLoadInstuctor = "Failed to load instructor info";
     }
 }
