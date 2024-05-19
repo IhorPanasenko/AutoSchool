337099a9ae4a215e36124c83c2d7f3e27d4f511a
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-05-19 13:13:52 +0300

    Create GetOneInstructorResponse.cs

---
+++ Auto.School.Mobile/Auto.School.Mobile.Core/Responses/Instructor/GetOne/GetOneInstructorResponse.cs
@@ -0,0 +1,12 @@
+﻿using Auto.School.Mobile.Core.Models;
+using Auto.School.Mobile.Core.Responses.Base;
+using Newtonsoft.Json;
+
+namespace Auto.School.Mobile.Core.Responses.Instructor.GetOne
+{
+    public class GetOneInstructorResponse : BaseResponse
+    {
+        [JsonProperty("data")]
+        public InstructorModel Instructor { get; set; }
+    }
+}
