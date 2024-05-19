07c23eca7b00f3fe78fa99c9b3a51fc4b7476ebd
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-05-19 13:12:27 +0300

    Add route constants

--- Auto.School.Mobile/Auto.School.Mobile.ApiIntegration/Constants/RoutesConstants.cs
+++ Auto.School.Mobile/Auto.School.Mobile.ApiIntegration/Constants/RoutesConstants.cs
@@ -3,12 +3,26 @@
     public static class RoutesConstants
     {
         public const string BaseUrl = "http://10.0.2.2:3000/api/";
-        public const string Login = "auth/login";
         public const string GetAllUsers = "users";
-        public const string GetAllCities = "cities";
+
+        #region Auth
+        public const string Login = "auth/login";
         public const string Register = "auth/signup";
-        public const string GetAllInstructors = "instructors";
-        public const string StudentSignUpToInstructor = "students/request-instructor";
         public const string ForgotPassword = "auth/forgotPassword";
+        #endregion
+
+        #region Student
+        public const string GetInfoMe = "students/me";
+        public const string StudentSignUpToInstructor = "students/request-instructor";
+        #endregion
+
+        #region City
+        public const string GetAllCities = "cities";
+        #endregion
+
+        #region Instructor
+        public const string GetAllInstructors = "instructors";
+        public const string GetOneInstructor = "instructors/";
+        #endregion
     }
 }
