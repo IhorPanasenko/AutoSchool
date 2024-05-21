ce68821fb0315f98548f51d44bc79fccdd9c6865
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-05-21 23:53:52 +0300

    Create UpdateMeResponse.cs

---
+++ Auto.School.Mobile/Auto.School.Mobile.Core/Responses/Student/UpdateMe/UpdateMeResponse.cs
@@ -0,0 +1,12 @@
+﻿using Auto.School.Mobile.Core.Models;
+using Auto.School.Mobile.Core.Responses.Base;
+using Newtonsoft.Json;
+
+namespace Auto.School.Mobile.Core.Responses.Student.UpdateMe
+{
+    public class UpdateMeResponse : BaseResponse
+    {
+        [JsonProperty("data")]
+        public StudentModel? Student { get; set; }
+    }
+}
