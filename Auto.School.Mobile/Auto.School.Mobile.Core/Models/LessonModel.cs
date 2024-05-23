b43dd7de2ad29f96fcb3d99b500caba3c3cea221
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-05-23 09:07:26 +0300

    Create LessonModel.cs

---
+++ Auto.School.Mobile/Auto.School.Mobile.Core/Models/LessonModel.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace Auto.School.Mobile.Core.Models
+{
+    public class LessonModel
+    {
+        [JsonProperty("_id")]
+        public string Id { get; set; }
+
+        [JsonProperty("instructorId")]
+        public string InstructorId { get; set; }
+
+        [JsonProperty("price")]
+        public int Price { get; set; }
+
+        [JsonProperty("date")]
+        public DateTime Date { get; set; }
+
+        [JsonProperty("fromHour")]
+        public string FromHour { get; set; }
+
+        [JsonProperty("toHour")]
+        public string ToHour { get; set; }
+
+        [JsonProperty("isAvailable")]
+        public bool IsAvailable { get; set; }
+    }
+}
