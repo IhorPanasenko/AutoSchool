05e8e38fbea174aad0ffcc1d88fb27409f6bd424
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-05-24 16:06:41 +0200

    Implement getting all reviews and/or for specific instructor

--- auto-school-server/src/controllers/reviewController.js
+++ auto-school-server/src/controllers/reviewController.js
@@ -2,7 +2,21 @@ const catchAsync = require('../helpers/catchAsync.js');
 const ReviewModel = require('../models/review.js');
 const StudentModel = require('../models/student.js');
 
-exports.getAllReviews = catchAsync(async (req, res, next) => {});
+exports.getAllReviews = catchAsync(async (req, res, next) => {
+  let filter = {};
+  if (req.params.instructorId)
+    filter = { instructorId: req.params.instructorId };
+
+  const reviews = await ReviewModel.find(filter);
+
+  res.status(200).json({
+    status: 'success',
+    results: reviews.length,
+    data: {
+      reviews,
+    },
+  });
+});
 
 exports.createReview = catchAsync(async (req, res, next) => {
   // TODO: Check if Student belongs to the Instructor
