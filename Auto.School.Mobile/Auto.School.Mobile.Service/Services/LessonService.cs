2f1ad9d7600c8037949f3b78d6f7558a7d68b5f7
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-05-25 02:16:22 +0300

    Create and implement LessonService

---
+++ Auto.School.Mobile/Auto.School.Mobile.Service/Services/LessonService.cs
@@ -0,0 +1,17 @@
+﻿using Auto.School.Mobile.ApiIntegration.Requests.Abstract;
+using Auto.School.Mobile.Core.Responses.Lesson.SignUp;
+using Auto.School.Mobile.Service.Interfaces;
+
+
+namespace Auto.School.Mobile.Service.Services
+{
+    public class LessonService(ILessonRequest lessonRequest) : ILessonService
+    {
+        private ILessonRequest _lessonRequest = lessonRequest;
+        public async Task<SignUpToLessonResponse> SignUpToLessonAsync(string lessonId)
+        {
+            var res = await _lessonRequest.SignUpToLesson(lessonId);
+            return res;
+        }
+    }
+}
