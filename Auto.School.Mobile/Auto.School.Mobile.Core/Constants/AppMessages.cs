509a794989b549ba8b8f63b47c5e199601d0a55b
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-05-24 09:03:21 +0300

    Add AppMessages

--- Auto.School.Mobile/Auto.School.Mobile.Core/Constants/AppMessages.cs
+++ Auto.School.Mobile/Auto.School.Mobile.Core/Constants/AppMessages.cs
@@ -2,12 +2,18 @@
 {
     public static class AppMessages
     {
+        #region ResetPassword
         public const string ResetPasswordSuccessMessage = "We sent you an email. Please, check your inbox or spam to reset your password";
+        #endregion
 
         #region StudentProfile
         public const string RequestNotSent = "You have not send request to connect to instructor yet.";
         public const string Pending = "You request haven't being confirming by our administration. Please wait for some time";
         public const string Rejected = "Your request was rejected. You can t send a request to iur another instructor.";
+        #endregion
+
+        #region SignUpToInstructor
+        public const string SignUpToInstructorSuccess = "You request was successfully sent. Wait while administrators will confirm it";
         #endregion
     }
 }
