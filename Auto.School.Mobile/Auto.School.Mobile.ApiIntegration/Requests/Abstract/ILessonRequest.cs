e6322d915227a05b05a0d5705acea7d47accd694
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-05-25 02:16:00 +0300

    Create and implement ILessonRequest

---
+++ Auto.School.Mobile/Auto.School.Mobile.ApiIntegration/Requests/Abstract/ILessonRequest.cs
@@ -0,0 +1,9 @@
+﻿using Auto.School.Mobile.Core.Responses.Lesson.SignUp;
+
+namespace Auto.School.Mobile.ApiIntegration.Requests.Abstract
+{
+    public interface ILessonRequest
+    {
+        public Task<SignUpToLessonResponse> SignUpToLesson(string lessonId);
+    }
+}
---
+++ Auto.School.Mobile/Auto.School.Mobile.ApiIntegration/Requests/Implementation/LessonRequest.cs
@@ -0,0 +1,19 @@
+﻿using Auto.School.Mobile.ApiIntegration.Base.Abstract;
+using Auto.School.Mobile.ApiIntegration.Constants;
+using Auto.School.Mobile.ApiIntegration.Helpers;
+using Auto.School.Mobile.ApiIntegration.Requests.Abstract;
+using Auto.School.Mobile.Core.Responses.Lesson.SignUp;
+
+namespace Auto.School.Mobile.ApiIntegration.Requests.Implementation
+{
+    public class LessonRequest(IPatchRequest patchRequest) : ILessonRequest
+    {
+        private readonly IPatchRequest _patchRequest = patchRequest;
+        public async Task<SignUpToLessonResponse> SignUpToLesson(string lessonId)
+        {
+            var url = FormUrlHelper.InsertIdIntoUrl(RoutesConstants.SignUpToLesson, lessonId);
+            var res = await _patchRequest.ExecuteAsync<object, SignUpToLessonResponse>(url);
+            return res;
+        }
+    }
+}
