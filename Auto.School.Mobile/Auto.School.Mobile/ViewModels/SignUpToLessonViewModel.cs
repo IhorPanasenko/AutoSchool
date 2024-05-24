d8cd5891e4e7db80f8e4eb0d7c18965b8bb42c42
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-05-25 02:17:08 +0300

    Create SignUpToLesson ViewModel

---
+++ Auto.School.Mobile/Auto.School.Mobile/ViewModels/SignUpToLessonViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Auto.School.Mobile.Abstract;
+using Auto.School.Mobile.Core.Constants;
+using Auto.School.Mobile.Core.Models;
+using Auto.School.Mobile.Service.Interfaces;
+using CommunityToolkit.Maui.Views;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.ComponentModel;
+
+namespace Auto.School.Mobile.ViewModels
+{
+    public partial class SignUpToLessonViewModel : BaseViewModel, INotifyPropertyChanged
+    {
+        private readonly ILessonService _lessonService;
+        private readonly ISharedService _sharedService;
+        public SignUpToLessonViewModel(ILessonService lessonService)
+        {
+            _lessonService = lessonService;
+            _ = GetInstructor();
+        }
+
+        private  async Task GetInstructor()
+        {
+            var lesson = _sharedService.GetValue<LessonModel>("SignUpLesson");
+            if(lesson is not null)
+            {
+                Lesson = lesson;
+            }
+        }
+
+        [ObservableProperty]
+        private Popup popupInstance;
+
+        [ObservableProperty]
+        private LessonModel lesson;
+
+        [RelayCommand]
+        public async Task SignUp()
+        {
+            var res = await _lessonService.SignUpToLessonAsync(Lesson.Id);
+
+            if (string.Compare(res.Status, ResponseStatuses.Sucess, true) == 0)
+            {
+                Lesson.IsAvailable = false;
+            }
+        }
+    }
+}
