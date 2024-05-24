9c91a65d8be20d65095860774db6c693498b337e
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-05-24 09:06:19 +0300

    Update InstructorScheduleStudentViewModel.cs

--- Auto.School.Mobile/Auto.School.Mobile/ViewModels/InstructorScheduleStudentViewModel.cs
+++ Auto.School.Mobile/Auto.School.Mobile/ViewModels/InstructorScheduleStudentViewModel.cs
@@ -5,6 +5,9 @@ using Auto.School.Mobile.Service.Interfaces;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System.ComponentModel;
+using System.Globalization;
+using Auto.School.Mobile.Core.Extensions;
+using Java.Lang.Reflect;
 
 namespace Auto.School.Mobile.ViewModels
 {
@@ -12,12 +15,14 @@ namespace Auto.School.Mobile.ViewModels
     {
         private readonly IInstructorService _instructorService;
         private readonly ISharedService _sharedService;
+        private DateTime _currentWeekStart;
 
         public InstructorScheduleStudentViewModel(IInstructorService instructorService, ISharedService sharedService)
         {
             _instructorService = instructorService;
             _sharedService = sharedService;
             CloseAction = CloseActionMethod;
+            _currentWeekStart = DateTime.Now.StartOfWeek(DayOfWeek.Monday);
             _ = LoadLessons();
         }
 
@@ -26,7 +31,7 @@ namespace Auto.School.Mobile.ViewModels
 
         private async Task LoadLessons()
         {
-            var instructorId = _sharedService.GetValue<string>("InstrctorId");
+            var instructorId = _sharedService.GetValue<string>("InstructorId");
             if (instructorId is null)
             {
                 IsLoading = false;
@@ -35,6 +40,7 @@ namespace Auto.School.Mobile.ViewModels
                 return;
             }
 
+            await LoadInstructor(instructorId);
             var response = await _instructorService.GetSchedule(instructorId);
 
             if (string.Compare(response.Status, ResponseStatuses.Fail) == 0)
@@ -47,6 +53,23 @@ namespace Auto.School.Mobile.ViewModels
 
             Lessons = response.Lessons;
         }
+        private async Task LoadInstructor(string instructorId)
+        {
+            var response = await _instructorService.GetOne(instructorId);
+
+            if (response is null || string.Compare(response.Status, ResponseStatuses.Fail, true) == 0)
+            {
+                IsError = true;
+                ErrorMessage = AppErrorMessagesConstants.FailedToLoadInstuctor;
+                return;
+            }
+
+            IsError = false;
+            Instructor = response.Instructor;
+        }
+
+        [ObservableProperty]
+        private InstructorModel instructor;
 
         [ObservableProperty]
         private bool isLoading = true;
@@ -66,9 +89,54 @@ namespace Auto.School.Mobile.ViewModels
         public Action CloseAction { get; set; }
 
         [RelayCommand]
+        public void LoadPreviousWeek()
+        {
+            _currentWeekStart = _currentWeekStart.AddDays(-7);
+            _ = LoadLessons();
+        }
+
+        [RelayCommand]
+        public void LoadNextWeek()
+        {
+            _currentWeekStart = _currentWeekStart.AddDays(7);
+            _ = LoadLessons();
+        }
+
+        [RelayCommand]
+        public async Task ViewDetailedInfo()
+        {
+            try
+            {
+                if (Instructor == null)
+                {
+                    return;
+                }
+
+                _sharedService.Add("instructor", Instructor);
+                await Shell.Current.GoToAsync(nameof(InstructorDetailsPage));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        [RelayCommand]
         public void SignUp(LessonModel lesson)
         {
 
         }
+
+        private bool IsLessonInCurrentWeek(LessonModel lesson)
+        {
+            var lessonDate = DateTime.ParseExact(lesson.FromHour, "yyyy-MM-ddTHH:mm:ss", CultureInfo.CurrentCulture);
+            return lessonDate >= _currentWeekStart && lessonDate < _currentWeekStart.AddDays(7);
+        }
+
+        public List<LessonModel> GetLessonsForDay(DayOfWeek dayOfWeek)
+        {
+            return Lessons.Where(lesson => lesson.DayOfWeek == dayOfWeek.ToString()).ToList();
+        }
+
     }
 }
