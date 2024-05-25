76930f851d6e6549144294552d1f11330d237ec9
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-05-25 18:27:25 +0300

    Call for request in service

--- Auto.School.Mobile/Auto.School.Mobile.Service/Interfaces/ILessonService.cs
+++ Auto.School.Mobile/Auto.School.Mobile.Service/Interfaces/ILessonService.cs
@@ -1,9 +1,12 @@
 ﻿using Auto.School.Mobile.Core.Responses.Lesson.SignUp;
+using Auto.School.Mobile.Core.Responses.Lesson.StudentGetMy;
 
 namespace Auto.School.Mobile.Service.Interfaces
 {
     public interface ILessonService
     {
         public Task<SignUpToLessonResponse> SignUpToLessonAsync(string lessonId);
+
+        public Task<StudentGetMyLessonsResponse> StudentGetMyLessonsAsync();
     }
 }
--- Auto.School.Mobile/Auto.School.Mobile.Service/Services/LessonService.cs
+++ Auto.School.Mobile/Auto.School.Mobile.Service/Services/LessonService.cs
@@ -1,5 +1,6 @@
 ﻿using Auto.School.Mobile.ApiIntegration.Requests.Abstract;
 using Auto.School.Mobile.Core.Responses.Lesson.SignUp;
+using Auto.School.Mobile.Core.Responses.Lesson.StudentGetMy;
 using Auto.School.Mobile.Service.Interfaces;
 
 
@@ -11,6 +12,12 @@ namespace Auto.School.Mobile.Service.Services
         public async Task<SignUpToLessonResponse> SignUpToLessonAsync(string lessonId)
         {
             var res = await _lessonRequest.SignUpToLesson(lessonId);
+            return res;
+        }
+
+        public async Task<StudentGetMyLessonsResponse> StudentGetMyLessonsAsync()
+        {
+            var res = await _lessonRequest.StudentGetMyLessons();
             return res;
         }
     }
