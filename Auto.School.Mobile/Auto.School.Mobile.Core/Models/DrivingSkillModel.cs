28d894714566a444d9590f3e32dcdca5feb9fdc5
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-05-29 01:58:11 +0300

    Create DrivingSkillModel.cs

---
+++ Auto.School.Mobile/Auto.School.Mobile.Core/Models/DrivingSkillModel.cs
@@ -0,0 +1,34 @@
+﻿using Auto.School.Mobile.Core.Constants;
+using Newtonsoft.Json;
+using System.Globalization;
+
+namespace Auto.School.Mobile.Core.Models
+{
+    public class DrivingSkillModel
+    {
+        [JsonProperty("completed")]
+        public bool Completed { get; set; }
+
+        [JsonProperty("_id")]
+        public string Id { get; set; } = string.Empty;
+
+        [JsonProperty("typeEN")]
+        public string TypeEN { get; set; } = string.Empty;
+
+        [JsonProperty("subtypeEN")]
+        public string SubtypeEN { get; set; } = string.Empty;
+
+        [JsonProperty("typeUA")]
+        public string TypeUA { get; set; } = string.Empty;
+
+        [JsonProperty("subtypeUA")]
+        public string SubtypeUA { get; set; } = string.Empty;
+
+        [JsonIgnore]
+        public string Type
+        {
+            get => string.Compare(CultureInfo.CurrentCulture.Name, LocalesConstants.Ukraine, true) == 0 ? TypeUA : TypeEN;
+            private set { }
+        }
+    }
+}
