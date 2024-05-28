6153ae1e75e428602855a51028ab4ba56bc7da1d
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-05-29 01:59:39 +0300

    Create StudentDrivingSkilllsViewModel.cs

---
+++ Auto.School.Mobile/Auto.School.Mobile/ViewModels/StudentDrivingSkilllsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Auto.School.Mobile.Abstract;
+using Auto.School.Mobile.Core.Models;
+using CommunityToolkit.Maui.Views;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel;
+
+namespace Auto.School.Mobile.ViewModels
+{
+    public partial class StudentDrivingSkilllsViewModel : BaseViewModel, INotifyPropertyChanged
+    {
+        private readonly ISharedService _sharedService;
+
+        public StudentDrivingSkilllsViewModel(ISharedService sharedService)
+        {
+            _sharedService = sharedService;
+        }
+
+        [ObservableProperty]
+        private Popup popupInstance;
+
+        [ObservableProperty]
+        private List<DrivingSkillModel> drivingSkills;
+
+
+    }
+}
