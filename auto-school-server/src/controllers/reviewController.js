d440a8ae5b64412c041c642ed9047e7b70d180e1
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-05-29 14:55:44 +0200

    Implement route for deleting review

--- auto-school-server/src/controllers/reviewController.js
+++ auto-school-server/src/controllers/reviewController.js
@@ -3,6 +3,7 @@ const ReviewModel = require('../models/review.js');
 const StudentModel = require('../models/student.js');
 const InstructorModel = require('../models/instructor.js');
 const AppError = require('../helpers/appError.js');
+const { findById } = require('../models/car.js');
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
   let filter = {};
@@ -41,5 +42,25 @@ exports.createReview = catchAsync(async (req, res, next) => {
     data: {
       review: newReview,
     },
+  });
+});
+
+exports.deleteReview = catchAsync(async (req, res, next) => {
+  if (req.user.role === 'student') {
+    const student = await StudentModel.findOne({ userId: req.user._id }).select(
+      '_id'
+    );
+    const review = await ReviewModel.findOneAndDelete({
+      _id: req.params.reviewId,
+      studentId: student._id,
+    });
+    if (!review)
+      return next(new AppError('You can not delete other person review', 403));
+  } else {
+    await ReviewModel.findByIdAndDelete(req.params.reviewId);
+  }
+
+  res.status(204).json({
+    status: 'success',
   });
 });
