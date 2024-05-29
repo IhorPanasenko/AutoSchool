393379c62c98cd2a1349494c3fcef0c1455e32b2
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-05-29 23:20:13 +0300

    Update AppErrorMessagesConstants.cs

--- Auto.School.Mobile/Auto.School.Mobile.Core/Constants/AppErrorMessagesConstants.cs
+++ Auto.School.Mobile/Auto.School.Mobile.Core/Constants/AppErrorMessagesConstants.cs
@@ -13,5 +13,6 @@
         public const string FailedLoadSchedule = "Failed to load instructor's schedule";
         public const string FailedUpdateUserPhoto = "Failed to update profile photo";
         public const string FiledSignUpToInstructor = "Failed Sign up to instructor";
+        public const string FailedToLoadDrivingSkills = "Failed to load your driving skills progress. Please, try again later";
     }
 }
