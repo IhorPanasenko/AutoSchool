40935b6faa4b59a3f736f44f17f366e3f7e98d25
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-05-30 02:01:55 +0300

    Create UpdateDrivingSkillsResponse.cs

---
+++ Auto.School.Mobile/Auto.School.Mobile.Core/Responses/Student/UpdateDrivingSkills/UpdateDrivingSkillsResponse.cs
@@ -0,0 +1,11 @@
+﻿using Auto.School.Mobile.Core.Responses.Base;
+using Newtonsoft.Json;
+
+namespace Auto.School.Mobile.Core.Responses.Student.UpdateDrivingSkills
+{
+    public class UpdateDrivingSkillsResponse : BaseResponse
+    {
+        [JsonProperty("data")]
+        public UpdateDrivingSkillsResponseData Data;
+    }
+}
