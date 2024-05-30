edfae2134802aa53588008aeb736329c8c44f87b
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-05-31 02:58:46 +0300

    Add reviewService

---
+++ Auto.School.Mobile/Auto.School.Mobile.Service/Interfaces/IReviewService.cs
@@ -0,0 +1,13 @@
+﻿using Auto.School.Mobile.Core.Models;
+using Auto.School.Mobile.Core.Responses.Base;
+using Auto.School.Mobile.Core.Responses.Review;
+
+namespace Auto.School.Mobile.Service.Interfaces
+{
+    public interface IReviewService
+    {
+        public Task<GetInstructorReviewResponse> GetInstructorReviews(string instructorId);
+
+        public Task<BaseResponse> AddReview(AddReviewModel model, string instructorId);
+    }
+}
---
+++ Auto.School.Mobile/Auto.School.Mobile.Service/Services/ReviewService.cs
@@ -0,0 +1,25 @@
+﻿using Auto.School.Mobile.ApiIntegration.Requests.Abstract;
+using Auto.School.Mobile.Core.Models;
+using Auto.School.Mobile.Core.Responses.Base;
+using Auto.School.Mobile.Core.Responses.Review;
+using Auto.School.Mobile.Service.Interfaces;
+
+namespace Auto.School.Mobile.Service.Services
+{
+    public class ReviewService(IReviewRequest reviewRequest) : IReviewService
+    {
+        private readonly IReviewRequest _reviewerRequest = reviewRequest;
+
+        public Task<BaseResponse> AddReview(AddReviewModel model, string instructorId)
+        {
+            var response = _reviewerRequest.AddReview(model, instructorId);
+            return response;
+        }
+
+        public Task<GetInstructorReviewResponse> GetInstructorReviews(string instructorId)
+        {
+            var res = _reviewerRequest.GetInstructorReview(instructorId);
+            return res;
+        }
+    }
+}
