1ed9d4508b1c99078c0d02a7fb5204457884c07a
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-05-31 02:57:57 +0300

    Create ReviewModel.cs

---
+++ Auto.School.Mobile/Auto.School.Mobile.Core/Models/ReviewModel.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace Auto.School.Mobile.Core.Models
+{
+    public class ReviewModel
+    {
+        [JsonProperty("_id")]
+        public int Id { get; set; }
+
+        [JsonProperty("review")]
+        public string Review { get; set; }
+
+        [JsonProperty("rating")]
+        public int Raiting { get; set; }
+
+        [JsonProperty("createdAt")]
+        public DateTime CreatedAt { get; set; }
+
+        [JsonProperty("instructorId")] 
+        public string InstructorId { get; set; }
+
+        [JsonProperty("studentId")]
+        public string StudentId { get; set; }
+    }
+}
