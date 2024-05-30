48c5ff4767deb14b3438cc59d8a9f21368560e0f
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-05-31 02:59:03 +0300

    Create InstructorReviewsViewModel.cs

---
+++ Auto.School.Mobile/Auto.School.Mobile/ViewModels/InstructorReviewsViewModel.cs
@@ -0,0 +1,54 @@
+﻿using Auto.School.Mobile.Abstract;
+using Auto.School.Mobile.Core.Constants;
+using Auto.School.Mobile.Core.Models;
+using Auto.School.Mobile.Service.Interfaces;
+using CommunityToolkit.Maui.Views;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel;
+
+
+namespace Auto.School.Mobile.ViewModels
+{
+    public partial class InstructorReviewsViewMode : BaseViewModel, INotifyPropertyChanged
+    {
+        private readonly ISharedService _sharedService;
+        private readonly IReviewService _reviewService;
+
+        public InstructorReviewsViewMode(IReviewService reviewService, ISharedService sharedService)
+        {
+            _sharedService = sharedService;
+            _reviewService = reviewService;
+            _ = LoadReviews();
+        }
+
+        private async Task LoadReviews()
+        {
+            var instructorId = _sharedService.GetValue<string>("InstructorId");
+
+            if(instructorId is null)
+            {
+                IsError = true;
+                ErrorMessage = AppErrorMessagesConstants.FailedGetInstructorId;
+                return;
+            }
+
+            var response =await _reviewService.GetInstructorReviews(instructorId);
+            if(string.Compare(response.Status, ResponseStatuses.Sucess) == 0)
+            {
+                Reviews = response.Reviews;
+            }
+        }
+
+        [ObservableProperty]
+        private Popup popUpInstance;
+
+        [ObservableProperty]
+        private List<ReviewModel> reviews;
+
+        [ObservableProperty]
+        private bool isError;
+
+        [ObservableProperty]
+        private string errorMessage;
+    }
+}
