62d105813aa59c2ca1bcedc4ab6c5dc1785d1b53
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-05-31 02:58:21 +0300

    Create models

---
+++ Auto.School.Mobile/Auto.School.Mobile.Core/Responses/Review/GetInstructorReviewResponse.cs
@@ -0,0 +1,12 @@
+﻿using Auto.School.Mobile.Core.Models;
+using Auto.School.Mobile.Core.Responses.Base;
+using Newtonsoft.Json;
+
+namespace Auto.School.Mobile.Core.Responses.Review
+{
+    public class GetInstructorReviewResponse : BaseResponse
+    {
+        [JsonProperty("data")]
+        public List<ReviewModel> Reviews { get; set; }
+    }
+}
