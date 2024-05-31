11cd9bbb59aedf68651cca320fb39a902f00d327
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-05-31 20:24:26 +0200

    Create Payment schema

---
+++ auto-school-server/src/models/payment.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const paymentSchema = new mongoose.Schema({
+  studentId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'students',
+    required: true,
+  },
+  lessonId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'lessons',
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  paytype: String,
+  timestamp: {
+    type: Date,
+    required: true,
+  },
+  provider: {
+    type: String,
+    default: 'LiqPay',
+  },
+});
+
+const PaymentModel = mongoose.model('payments', paymentSchema);
+
+module.exports = PaymentModel;
