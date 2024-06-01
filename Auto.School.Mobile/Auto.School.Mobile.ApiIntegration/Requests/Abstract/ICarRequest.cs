19d36876c467198e0f2b8b4de4a2fac525f80edf
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-06-01 14:47:30 +0300

    Add and implement Car request

---
+++ Auto.School.Mobile/Auto.School.Mobile.ApiIntegration/Requests/Abstract/ICarRequest.cs
@@ -0,0 +1,9 @@
+﻿using Auto.School.Mobile.Core.Responses.Base;
+
+namespace Auto.School.Mobile.ApiIntegration.Requests.Abstract
+{
+    public interface ICarRequest
+    {
+        public Task<BaseResponse> AddRating(string carId, int rating);
+    }
+}
