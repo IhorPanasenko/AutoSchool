8978037e410c56310a1aec32a29fa0eee898da8b
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-06-01 03:05:35 +0300

    Update InstructorReviewsPopUp.xaml.cs

--- Auto.School.Mobile/Auto.School.Mobile/Views/InstructorReviewsPopUp.xaml.cs
+++ Auto.School.Mobile/Auto.School.Mobile/Views/InstructorReviewsPopUp.xaml.cs
@@ -1,11 +1,14 @@
+using Auto.School.Mobile.ViewModels;
 using CommunityToolkit.Maui.Views;
 
 namespace Auto.School.Mobile.Views;
 
 public partial class InstructorReviewsPopUp : Popup
 {
-	public InstructorReviewsPopUp()
+	public InstructorReviewsPopUp(InstructorReviewsViewMode instructorReviewsViewMode)
 	{
 		InitializeComponent();
+		BindingContext = instructorReviewsViewMode;
+		instructorReviewsViewMode.PopUpInstance = this;
 	}
 }
