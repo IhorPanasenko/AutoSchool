c42af998a35e1bbe0a4a6cabb83c3b062f2ab58c
Ihor Panasenko <ihor.panasenko1@nure.ua>
2024-06-01 03:05:01 +0300

    Update InstructorReviewsViewModel.cs

--- Auto.School.Mobile/Auto.School.Mobile/ViewModels/InstructorReviewsViewModel.cs
+++ Auto.School.Mobile/Auto.School.Mobile/ViewModels/InstructorReviewsViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using Auto.School.Mobile.Abstract;
 using Auto.School.Mobile.Core.Constants;
 using Auto.School.Mobile.Core.Models;
+using Auto.School.Mobile.Core.Responses.Auth.Login;
 using Auto.School.Mobile.Service.Interfaces;
 using CommunityToolkit.Maui.Views;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Newtonsoft.Json;
 using System.ComponentModel;
 
 
@@ -24,6 +27,7 @@ namespace Auto.School.Mobile.ViewModels
         private async Task LoadReviews()
         {
             var instructorId = _sharedService.GetValue<string>("InstructorId");
+            var isSignedUpToInstructor = (bool)_sharedService.GetValue<object>("IsSignedUpToInstructor")!;
 
             if(instructorId is null)
             {
@@ -33,9 +37,21 @@ namespace Auto.School.Mobile.ViewModels
             }
 
             var response =await _reviewService.GetInstructorReviews(instructorId);
-            if(string.Compare(response.Status, ResponseStatuses.Sucess) == 0)
+            if(string.Compare(response.Status, ResponseStatuses.Sucess, true) == 0)
             {
-                Reviews = response.Reviews;
+                Reviews = response.Data?.Reviews ?? new List<ReviewModel>();
+                Reviews = Reviews.OrderByDescending(r => r.CreatedAt).ToList();
+            }
+
+            if (isSignedUpToInstructor)
+            {
+                string? userInfoJson = Preferences.Get("UserInfo", null);
+
+                if (!string.IsNullOrEmpty(userInfoJson))
+                {
+                    var userLoginInfo = JsonConvert.DeserializeObject<LoginResponseData>(userInfoJson);
+                    IsMyReviewExists = Reviews.Any(r => r.StudentId.Id == userLoginInfo?.UserData.Id);
+                }  
             }
         }
 
@@ -50,5 +66,14 @@ namespace Auto.School.Mobile.ViewModels
 
         [ObservableProperty]
         private string errorMessage;
+
+        [ObservableProperty]
+        private bool isMyReviewExists;
+
+        [RelayCommand]
+        public void DeleteMyReview()
+        {
+            
+        }
     }
 }
