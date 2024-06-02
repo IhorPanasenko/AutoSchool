fcf240ada75912def88b62fa66cc86f98855f3e0
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-06-02 12:49:18 +0200

    Create schema `chatMessage` schema

---
+++ auto-school-server/src/models/chatMessage.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const chatMessageSchema = new mongoose.Schema({
+  fromUser: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'userAccounts',
+    required: [true, 'fromUser id is required'],
+  },
+  toUser: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'userAccounts',
+    required: [true, 'toUser id is required'],
+  },
+  text: {
+    type: String,
+    required: [true, 'Content is required'],
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const ChatMessageModel = mongoose.model('chatMessages', chatMessageSchema);
+
+module.exports = ChatMessageModel;
