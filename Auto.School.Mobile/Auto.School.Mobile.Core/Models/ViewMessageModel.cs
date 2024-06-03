4bed288e9e8418abebe6b9f328c4c09ce1acde51
Ihor panasenko <ihor.panasenko1@nure.ua>
2024-06-03 20:22:43 +0300

    Create ViewMessageModel.cs

---
+++ Auto.School.Mobile/Auto.School.Mobile.Core/Models/ViewMessageModel.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Auto.School.Mobile.Core.Models
+{
+    public  class ViewMessageModel
+    {
+        [JsonProperty("_id")]
+        public int Id { get; set; }
+
+        [JsonProperty("fromUser")]
+        public string FromUser {  get; set; }
+
+        [JsonProperty("toUser")]
+        public string ToUser { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+
+        [JsonProperty("timestmap")]
+        public DateTime SendingTime { get; set; }
+    }
+}
