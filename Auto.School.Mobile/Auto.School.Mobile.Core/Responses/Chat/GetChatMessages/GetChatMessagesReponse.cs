40952e7aad159df08f32f1fed8222968428d5ca6
Ihor panasenko <ihor.panasenko1@nure.ua>
2024-06-03 20:22:58 +0300

    Create Get Chat MEssagesResponse

---
+++ Auto.School.Mobile/Auto.School.Mobile.Core/Responses/Chat/GetChatMessages/GetChatMessagesReponse.cs
@@ -0,0 +1,11 @@
+﻿using Auto.School.Mobile.Core.Responses.Base;
+using Newtonsoft.Json;
+
+namespace Auto.School.Mobile.Core.Responses.Chat.GetChatMessages
+{
+    public class GetChatMessagesReponse : BaseResponse
+    {
+        [JsonProperty("data")]
+        public GetChatMessagesResponseData? Data { get; set; }
+    }
+}
