04384b5c0241426f27d4573768810561e5308ebc
Ihor panasenko <ihor.panasenko1@nure.ua>
2024-06-03 20:23:50 +0300

    Create InstructorAllChatsViewModel.cs

---
+++ Auto.School.Mobile/Auto.School.Mobile/ViewModels/Instructor/InstructorAllChatsViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+
+namespace Auto.School.Mobile.ViewModels.Instructor
+{
+    public partial class InstructorAllChatsViewModel : BaseViewModel, INotifyPropertyChanged
+    {
+        private readonly IChatsService _chatsService;
+    }
+}
