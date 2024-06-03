0e0ee40c05183f22327030176bb5d5591b668ecf
Ihor panasenko <ihor.panasenko1@nure.ua>
2024-06-03 20:23:59 +0300

    Create ChatPreviewModel.cs

---
+++ Auto.School.Mobile/Auto.School.Mobile.Core/Models/ChatPreviewModel.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace Auto.School.Mobile.Core.Models
+{
+    public class ChatPreviewModel
+    {
+        [JsonProperty("_id")]
+        public string? Id { get; set; }
+
+        [JsonProperty("name")]
+        public string? Name { get; set; }
+
+        [JsonProperty("surname")]
+        public string? Surname { get; set; }
+
+        [JsonProperty("userId")]
+        public string? UserId { get; set; }
+
+        [JsonProperty("photoUrl")]
+        public string? PhotoUrl { get; set; }
+
+        [JsonProperty("lastMessage")]
+        public string? LastMessage { get; set; }
+    }
+}
