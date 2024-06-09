3e85d5a4284ca3cdc3073cb62b51155699324923
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-06-09 15:12:40 +0200

    test(authenticateJWT): add unit test for valid access token provided

--- auto-school-server/src/__tests__/middleware/authJWT.test.js
+++ auto-school-server/src/__tests__/middleware/authJWT.test.js
@@ -5,7 +5,7 @@ const UserAccountModel = require('../../models/userAccount.js');
 const UserLoginModel = require('../../models/userLogin.js');
 
 describe('authenticateJWT', () => {
-  let req, res, next;
+  let res, next;
   beforeEach(() => {
     req = {
       cookies: {},
@@ -30,22 +30,86 @@ describe('authenticateJWT', () => {
     it('should return status 403', () => {
       req.cookies.access_token = 'invalid_access_token';
 
-      jwt.verify = jest
-        .fn()
-        .mockImplementationOnce((token, secret, callback) => {
-          callback(new Error('Invalid token'));
-        });
+      jwt.verify = jest.fn().mockImplementationOnce(() => {
+        throw new Error('Invalid token');
+      });
 
       authenticateJWT(req, res, next);
 
       expect(jwt.verify).toHaveBeenCalledWith(
         req.cookies.access_token,
-        process.env.JWT_ACCESS_TOKEN_SECRET,
-        expect.any(Function)
+        process.env.JWT_ACCESS_TOKEN_SECRET
       );
       expect(res.status).toHaveBeenCalledWith(403);
       expect(res.json).toHaveBeenCalledWith({
         error: 'Access token is no longer valid',
+      });
+      expect(next).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('given valid access token', () => {
+    it('should attach user to req', async () => {
+      const userId = 'user123';
+      const accessToken = jwt.sign(
+        { userId },
+        process.env.JWT_ACCESS_TOKEN_SECRET
+      );
+      req.cookies.access_token = accessToken;
+      jwt.verify = jest.fn().mockImplementationOnce(() => ({ userId }));
+
+      const mockCurrentUser = { _id: userId, role: 'student' };
+      const mockCurrentUserLoginData = {
+        passwordChangedAfter: jest.fn(() => false),
+      };
+      jest
+        .spyOn(UserAccountModel, 'findById')
+        .mockResolvedValueOnce(mockCurrentUser);
+      jest
+        .spyOn(UserLoginModel, 'findOne')
+        .mockResolvedValueOnce(mockCurrentUserLoginData);
+
+      await authenticateJWT(req, res, next);
+
+      expect(UserAccountModel.findById).toHaveBeenCalledWith(userId);
+      expect(UserLoginModel.findOne).toHaveBeenCalledWith({ userId });
+      expect(req.user).toEqual(mockCurrentUser);
+      expect(res.status).not.toHaveBeenCalled();
+      expect(res.json).not.toHaveBeenCalled();
+      expect(next).toHaveBeenCalled();
+    });
+
+    it('should return status 401 if password was changed', async () => {
+      const userId = 'user123';
+      const iat = 'some_date';
+      const accessToken = jwt.sign(
+        { userId },
+        process.env.JWT_ACCESS_TOKEN_SECRET
+      );
+      req.cookies.access_token = accessToken;
+      jwt.verify = jest.fn().mockImplementationOnce(() => ({ userId, iat }));
+
+      const mockCurrentUser = { _id: userId, role: 'student' };
+      const mockCurrentUserLoginData = {
+        passwordChangedAfter: jest.fn(() => true),
+      };
+      jest
+        .spyOn(UserAccountModel, 'findById')
+        .mockResolvedValueOnce(mockCurrentUser);
+      jest
+        .spyOn(UserLoginModel, 'findOne')
+        .mockResolvedValueOnce(mockCurrentUserLoginData);
+
+      await authenticateJWT(req, res, next);
+
+      expect(UserAccountModel.findById).toHaveBeenCalledWith(userId);
+      expect(UserLoginModel.findOne).toHaveBeenCalledWith({ userId });
+      expect(
+        mockCurrentUserLoginData.passwordChangedAfter
+      ).toHaveBeenCalledWith(iat);
+      expect(res.status).toHaveBeenCalledWith(401);
+      expect(res.json).toHaveBeenCalledWith({
+        error: 'Password was changed. Please, log in again',
       });
       expect(next).not.toHaveBeenCalled();
     });
--- auto-school-server/src/middlewares/authenticateJWT.js
+++ auto-school-server/src/middlewares/authenticateJWT.js
@@ -2,44 +2,70 @@ const jwt = require('jsonwebtoken');
 const UserAccount = require('../models/userAccount.js');
 const UserLogin = require('../models/userLogin.js');
 
-exports.authenticateJWT = (req, res, next) => {
+exports.authenticateJWT = async (req, res, next) => {
   const accessToken = req.cookies.access_token;
 
   if (!accessToken) {
     return res.status(401).json({ error: 'You are not logged in' });
   }
 
-  jwt.verify(
-    accessToken,
-    process.env.JWT_ACCESS_TOKEN_SECRET,
-    async (err, data) => {
-      if (err) {
-        return res.status(403).json({
-          error: 'Access token is no longer valid',
-        });
-      }
+  try {
+    const data = jwt.verify(accessToken, process.env.JWT_ACCESS_TOKEN_SECRET);
 
-      const currentUser = await UserAccount.findById(data.userId);
+    const currentUser = await UserAccount.findById(data.userId);
 
-      if (!currentUser) {
-        return res.status(400).json({ error: "User doesn't exist" });
-      }
-
-      const currentUserLoginData = await UserLogin.findOne({
-        userId: data.userId,
-      });
-      const passwordChanged = currentUserLoginData.passwordChangedAfter(
-        data.iat
-      );
-
-      if (passwordChanged) {
-        return res
-          .status(401)
-          .json({ error: 'Password was changed. Please, log in again' });
-      }
-
-      req.user = currentUser;
-      next();
+    if (!currentUser) {
+      return res.status(400).json({ error: "User doesn't exist" });
     }
-  );
+
+    const currentUserLoginData = await UserLogin.findOne({
+      userId: data.userId,
+    });
+    const passwordChanged = currentUserLoginData.passwordChangedAfter(data.iat);
+
+    if (passwordChanged) {
+      return res
+        .status(401)
+        .json({ error: 'Password was changed. Please, log in again' });
+    }
+
+    req.user = currentUser;
+    next();
+  } catch (err) {
+    return res.status(403).json({
+      error: 'Access token is no longer valid',
+    });
+  }
+  //   accessToken,
+  //   process.env.JWT_ACCESS_TOKEN_SECRET,
+  //   async (err, data) => {
+  //     if (err) {
+  //       return res.status(403).json({
+  //         error: 'Access token is no longer valid',
+  //       });
+  //     }
+  //     const currentUser = await UserAccount.findById(data.userId);
+
+  //     if (!currentUser) {
+  //       return res.status(400).json({ error: "User doesn't exist" });
+  //     }
+
+  //     const currentUserLoginData = await UserLogin.findOne({
+  //       userId: data.userId,
+  //     });
+  //     const passwordChanged = currentUserLoginData.passwordChangedAfter(
+  //       data.iat
+  //     );
+
+  //     if (passwordChanged) {
+  //       console.log('Password changed');
+  //       return res
+  //         .status(401)
+  //         .json({ error: 'Password was changed. Please, log in again' });
+  //     }
+
+  //     req.user = currentUser;
+  //     next();
+  //   }
+  // );
 };
