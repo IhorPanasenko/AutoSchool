e15084e901e98c643a71406cdb2b16b8d32a80ae
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-06-09 12:21:33 +0200

    test(middleware): add unit test for restrictTo when given non-permitter roles

--- auto-school-server/src/__tests__/middleware/restrictTo.test.js
+++ auto-school-server/src/__tests__/middleware/restrictTo.test.js
@@ -1,10 +1,17 @@
 const { restrictTo } = require('../../middlewares/restrictTo.js');
 const AppError = require('../../helpers/appError.js');
 
-const res = {};
-const next = jest.fn();
+// const res = {};
+// const next = jest.fn();
 
 describe('restrictTo', () => {
+  beforeEach(() => {
+    res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+    next = jest.fn();
+  });
   describe('given permitted roles', () => {
     it('it should allow access', () => {
       const req = {
@@ -15,6 +22,24 @@ describe('restrictTo', () => {
       middleware(req, res, next);
 
       expect(next).toHaveBeenCalled();
+    });
+  });
+
+  describe('given non-permitted roles', () => {
+    it('it should restrict access', () => {
+      const req = {
+        user: { role: 'student' },
+      };
+
+      const middleware = restrictTo('admin');
+      middleware(req, res, next);
+
+      expect(next).toHaveBeenCalledWith(expect.any(AppError));
+      const errorInstance = next.mock.calls[0][0];
+      expect(errorInstance.message).toBe(
+        'You have no permission to perform this action'
+      );
+      expect(errorInstance.statusCode).toBe(403);
     });
   });
 });
