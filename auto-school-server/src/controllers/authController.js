e35fb5d9e655cb0e4d0952fb3efe6e1e776534cf
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-06-15 16:43:36 +0200

    Remove comments

--- auto-school-server/src/controllers/authController.js
+++ auto-school-server/src/controllers/authController.js
@@ -14,41 +14,6 @@ const {
   peopleApi,
 } = require('../config/googleOauth2Client.js');
 const { signSaveTokens } = require('../authUtils/signSaveTokens.js');
-//   const accessToken = jwt.sign(
-//     { userId, role },
-//     process.env.JWT_ACCESS_TOKEN_SECRET,
-//     {
-//       expiresIn: process.env.JWT_ACCESS_TOKEN_EXPIRES,
-//     }
-//   );
-//   const refreshToken = jwt.sign(
-//     { userId },
-//     process.env.JWT_REFRESH_TOKEN_SECRET,
-//     {
-//       expiresIn: process.env.JWT_REFRESH_TOKEN_EXPIRES,
-//     }
-//   );
-
-//   const accessTokenCookieExpireDate = new Date(
-//     Date.now() + process.env.JWT_ACCESS_TOKEN_COOKIE_EXPIRES * 1000
-//   );
-
-//   res.cookie('access_token', accessToken, {
-//     expire: accessTokenCookieExpireDate,
-//     httpOnly: true,
-//     sameSite: 'None',
-//     secure: true,
-//   });
-
-//   res.cookie('refresh_token', refreshToken, {
-//     maxAge: process.env.JWT_REFRESH_TOKEN_COOKIE_EXPIRES * 24 * 60 * 60 * 1000,
-//     httpOnly: true,
-//     sameSite: 'None',
-//     secure: true,
-//   });
-
-//   return { accessToken, refreshToken, expire: accessTokenCookieExpireDate };
-// };
 
 const sendVerificationEmailToken = async (req, userLogin, name) => {
   const token = randomString();
